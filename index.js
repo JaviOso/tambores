71cc32663c2878917c3bb9ea713a2b3584e3de0d
JaviOso <45696865+JaviOso@users.noreply.github.com>
2021-09-01 21:17:40 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,54 @@
+for(var i = 0;i < 7;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    var buttonHTML=this.innerHTML;
+    makeNoise(buttonHTML);
+    buttonAnimation(buttonHTML);
+  });
+}
+
+document.addEventListener("keydown",function (event){
+  makeNoise(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeNoise(key){
+  switch(key){
+    case "w":
+      var audio=new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio=new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio=new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio=new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio=new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio=new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio=new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+  }
+}
+
+function buttonAnimation(keyA){
+  var activeButton = document.querySelector("."+keyA);
+  activeButton.classList.add("pressed");
+  setTimeout(function (){
+    activeButton.classList.remove("pressed");
+  },100);
+}
